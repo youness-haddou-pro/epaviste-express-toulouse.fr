62e6e3d43ee83c4f700e076e8f3c7fe37df6e94b
Younesshs <youness.haddou@outlook.fr>
2025-08-19 14:52:53 +0200

    Petite modification SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
+	site: "https://www.epaviste-express-toulouse.fr",
 	vite: {
 		plugins: [tailwindcss()],
 	},
